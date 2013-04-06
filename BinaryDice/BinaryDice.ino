9d435aad5a1f01dbd3bdce85be6f0950b9a5a199
Amo Wu <amo260@gmail.com>
2013-04-06 15:13:53 +0800

    Add BinaryDice comment.

--- BinaryDice/BinaryDice.ino
+++ BinaryDice/BinaryDice.ino
@@ -1,5 +1,27 @@
 /**
- * This is a simple Arduino dice game.
+ * This is a simple Arduino project to implement a binary dice guessing game.
+ *
+ * How to play
+ * -----------
+ *
+ * 1. There are three LEDs on the board representing the binary LED_BIT0, LED_BIT1, LED_BIT2, so it can display 0(000)-7(111).
+ * 2. Click GUESS_BUTTON_PIN to select the number you want to guess, and show on LEDs.
+ * 3. Click START_BUTTON_PIN to start random, if you guessed, the LED will blink.
+ *
+ * Used components
+ * ---------------
+ *
+ * - Arduino UNO x 1
+ * - Breadboard x 1
+ * - LED x 3
+ * - Pushbutton x 2
+ * - Resistance 10kΩ x 2
+ * - Resistance 1kΩ x 4
+ *
+ * Used library
+ * ------------
+ *
+ * - [Bounce](http://playground.arduino.cc/code/bounce), Bounce is a library for Arduino (arduino.cc). It debounces digital inputs and more.
  */
 #include <Bounce.h>
 
